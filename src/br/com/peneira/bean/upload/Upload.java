7a4bae7eadb89a4c44981cecbffc71104bbda363
Bruno Casali <brunoocasali@gmail.com>
2014-12-04 11:58:56 -0200

    commited to fucker project

--- src/br/com/peneira/bean/upload/Upload.java
+++ src/br/com/peneira/bean/upload/Upload.java
@@ -3,30 +3,27 @@ package br.com.peneira.bean.upload;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import br.com.peneira.vo.VideoVO;
-import br.edu.unisep.hibernate.GenericDAO;
-
 @ManagedBean(name = "NewUploadBean")
 @ViewScoped
 public class Upload {
-	private VideoVO video = new VideoVO();
-
-	public String save() {
-
-		GenericDAO<VideoVO> dao = new GenericDAO<VideoVO>();
-		if (video.getId() != null && video.getId() != 0) {
-			dao.update(video);
-		} else {
-			dao.save(video);
-		}
-		return "new_video";
-	}
-
-	public VideoVO getVideos() {
-		return video;
-	}
-
-	public void setVideos(VideoVO video) {
-		this.video = video;
-	}
+//	private VideoVO video = new VideoVO();
+//
+//	public String save() {
+//
+//		GenericDAO<VideoVO> dao = new GenericDAO<VideoVO>();
+//		if (video.getId() != null && video.getId() != 0) {
+//			dao.update(video);
+//		} else {
+//			dao.save(video);
+//		}
+//		return "new_video";
+//	}
+//
+//	public VideoVO getVideos() {
+//		return video;
+//	}
+//
+//	public void setVideos(VideoVO video) {
+//		this.video = video;
+//	}
 }
